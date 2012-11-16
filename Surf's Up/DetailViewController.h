3e0f3977f695d43c24e2fe0465800c6322fb5087
sursrc <sursrc@gmail.com>
2012-11-16 12:18:27 +0800

    complete

---
+++ Surf's Up/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Surf's Up
+//
+//  Created by Baranski Steve on 7/1/11.
+//  Copyright 2011 komorka technology, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ Surf's Up/PlaceholderViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaceholderViewController.h
+//  Surf's Up
+//
+//  Created by Baranski Steve on 7/1/11.
+//  Copyright 2011 komorka technology, llc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaceholderViewController : UIViewController
+
+@end
